05d69f07d32a21530c819972d0376b68916b98c3
Chester Husk III <chusk3@gmail.com>
2012-02-16 17:42:50 -0600

    Initial commit with the first revision of the ProcessorConfiguration, as well as a serializer for those Configurations.  Also set up a basic gitignore that will prevent user settings from entering the project. Finally, added in project struture and initial library dependencies of JUnit and XStream.

---
+++ src/utility/Serializer.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package utility;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import model.ProcessorConfiguration;
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * @author chusk3
+ *
+ * This class serializes objects, primarily the Processor Configuration, via the xstream library.
+ */
+public class Serializer {
+
+	protected static XStream initXStream()
+	{
+		XStream streamer = new XStream();
+		
+		// Doing this makes the XML not have to worry with namespaces.
+		streamer.processAnnotations(ProcessorConfiguration.class);
+		return streamer;
+	}
+	
+	/**
+	 * Given a processor configuration, returns the xml string that describes that configuration.
+	 * @param config
+	 * @return
+	 */
+	public static String serializeConfiguration(ProcessorConfiguration config)
+	{
+		return initXStream().toXML(config);
+	}
+	
+	/**
+	 * Writes a config to a specified File.
+	 * @param config
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean serializeConfigTo(File file, ProcessorConfiguration config) throws IOException {
+		FileWriter writer = new FileWriter(file);
+		writer.write(initXStream().toXML(config));
+		writer.close();
+		return true;
+	}
+
+	/**
+	 * Writes a config to a specified file Path.
+	 * @param config
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean serializeConfigTo(String filePath, ProcessorConfiguration config) throws IOException {
+		FileWriter writer = new FileWriter(filePath);
+		writer.write(initXStream().toXML(config));
+		writer.close();
+		return true;
+	}
+
+	/**
+	 * Given an xml string that describes a Processor Configuration, returns that configuration.
+	 * @param xml
+	 * @return
+	 */
+	public static ProcessorConfiguration deserializeMapFrom(String xml){
+		return (ProcessorConfiguration) initXStream().fromXML(xml);
+	}
+
+	/**
+	 * Given a File, returns the ProcessorConfiguration described by that File.
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 */
+	public static ProcessorConfiguration deserializeConfigFrom(File file) throws IOException {
+		FileReader reader = new FileReader(file);
+		ProcessorConfiguration result = (ProcessorConfiguration)initXStream().fromXML(reader);
+		reader.close();
+		return result;
+	}
+
+	/**
+	 * Given a file path, returns the ProcessorConfiguration described at that path.
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
+	public static ProcessorConfiguration deserializeConfigFrom(String filePath) throws IOException {
+		FileReader reader = new FileReader(filePath);
+		ProcessorConfiguration result = (ProcessorConfiguration)initXStream().fromXML(reader);
+		reader.close();
+		return result;
+	}
+}

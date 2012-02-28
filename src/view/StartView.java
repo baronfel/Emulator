eefd50a619e13ef51e66bed48b195159b97c6d76
Chester Husk III <chusk3@gmail.com>
2012-02-27 18:48:52 -0600

    Added SWT library to project and updated references.  Will begin UI work now.

---
+++ src/view/StartView.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+package view;
+
+import org.eclipse.swt.widgets.*;
+
+
+/**
+ * @author Chet
+ *
+ * This class is the entry point for the app.  When this is run, the main UI is set up and shown to the user.
+ */
+public class StartView {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

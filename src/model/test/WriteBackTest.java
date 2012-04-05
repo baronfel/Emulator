e424848df236bfe4c558b663157a2c6fe365b684
Chester Husk III <chusk3@gmail.com>
2012-04-05 18:23:23 -0500

    Tweaking registry to allow fo PC, LO, and HI. also tweaked WritebackTest.

--- src/model/test/WriteBackTest.java
+++ src/model/test/WriteBackTest.java
@@ -1,6 +1,11 @@
 package model.test;
 
 import static org.junit.Assert.*;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -19,13 +24,19 @@ public class WriteBackTest {
 
   private Map<String, Integer> opCycles;
   private List<IALU> alus = new ArrayList<IALU>(2);
-  private Memory memoryBanks = new Memory(100);
-  private IMemoryAccess memory = new MemoryAccess(102, 4, 444);
+  private Mockery mocker = new Mockery();
+  private final IMemoryAccess memory = mocker.mock(IMemoryAccess.class);
   private Registry registers = new Registry();
+  
   
   @Test
   public void ALUListProcessesCorrectly()
   {
+	  
+	  mocker.checking(new Expectations(){{
+		oneOf(memory.getPostMEMSequenceNum(false)); returnValue(-1);  
+	  }});
+	  
    alus.add(new ALU(100, 2, 222));
    alus.add(new ALU(101, 3, 333));
    WriteBack writeBack = new WriteBack(memory, alus, registers); 
@@ -37,6 +48,13 @@ public class WriteBackTest {
   @Test
   public void MemoryAccessProcessesCorrectly()
   {
+	  mocker.checking(new Expectations(){{
+		  oneOf(memory.getPostMEMSequenceNum(false)); returnValue(0);
+		  oneOf(memory.getPostMEMSequenceNum(true)); returnValue(0);
+		  oneOf(memory.getPostMEMDestReg()); returnValue(4);
+		  oneOf(memory.getPostMEMOpResult()); returnValue(444);
+	  }});
+	  
    WriteBack writeBack = new WriteBack(memory, alus, registers); 
    writeBack.processClockCycle(); 
    assertEquals(registers.getValue(4), 444); 

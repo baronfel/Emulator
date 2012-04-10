18f143629377b22441c98e009364641073f94659
Chester Husk III <chusk3@gmail.com>
2012-04-10 17:52:01 -0500

    cleanup.

--- src/model/test/WriteBackTest.java
+++ src/model/test/WriteBackTest.java
@@ -1,28 +1,25 @@
 package model.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import interfaces.IALU;
 import interfaces.IMemoryAccess;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import model.ALU;
 import model.Registry;
 import model.WriteBack;
 
-import org.jmock.Expectations;
-import org.jmock.Mockery;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class WriteBackTest {
 
-	private Map<String, Integer> opCycles;
 	private List<IALU> alus = new ArrayList<IALU>(2);
-	private final IMemoryAccess memory = Mockito.mock(IMemoryAccess.class);
+	private final IMemoryAccess memory = mock(IMemoryAccess.class);
 	private Registry registers = new Registry();
 
 	@Before
@@ -33,15 +30,15 @@ public class WriteBackTest {
 	@Test
 	public void ALUListProcessesCorrectly() {
 
-		Mockito.when(memory.getPostMEMSequenceNum(false)).thenReturn(-1);
-		IALU fake1 = Mockito.mock(IALU.class);
-		Mockito.when(fake1.getPostALUDestReg()).thenReturn(2);
-		Mockito.when(fake1.getPostALUOpResult()).thenReturn(222);
-		Mockito.when(fake1.getPostALUSequenceNum(Mockito.anyBoolean())).thenReturn(100);
-		IALU fake2 = Mockito.mock(IALU.class);
-		Mockito.when(fake2.getPostALUDestReg()).thenReturn(3);
-		Mockito.when(fake2.getPostALUOpResult()).thenReturn(333);
-		Mockito.when(fake2.getPostALUSequenceNum(Mockito.anyBoolean())).thenReturn(101);
+		when(memory.getPostMEMSequenceNum(false)).thenReturn(-1);
+		IALU fake1 = mock(IALU.class);
+		when(fake1.getPostALUDestReg()).thenReturn(2);
+		when(fake1.getPostALUOpResult()).thenReturn(222);
+		when(fake1.getPostALUSequenceNum(anyBoolean())).thenReturn(100);
+		IALU fake2 = mock(IALU.class);
+		when(fake2.getPostALUDestReg()).thenReturn(3);
+		when(fake2.getPostALUOpResult()).thenReturn(333);
+		when(fake2.getPostALUSequenceNum(anyBoolean())).thenReturn(101);
 		alus.add(fake1);
 		alus.add(fake2);
 		
@@ -55,11 +52,11 @@ public class WriteBackTest {
 	@Test
 	public void MemoryAccessProcessesCorrectly() {
 		
-		IALU fakeALU = Mockito.mock(IALU.class);
-		Mockito.when(fakeALU.getPostALUSequenceNum(false)).thenReturn(-1);
-		Mockito.when(memory.getPostMEMSequenceNum(true)).thenReturn(0);
-		Mockito.when(memory.getPostMEMDestReg()).thenReturn(4);
-		Mockito.when(memory.getPostMEMOpResult()).thenReturn(444);
+		IALU fakeALU = mock(IALU.class);
+		when(fakeALU.getPostALUSequenceNum(false)).thenReturn(-1);
+		when(memory.getPostMEMSequenceNum(true)).thenReturn(0);
+		when(memory.getPostMEMDestReg()).thenReturn(4);
+		when(memory.getPostMEMOpResult()).thenReturn(444);
 		alus.add(fakeALU);
 
 		WriteBack writeBack = new WriteBack(memory, alus, registers);

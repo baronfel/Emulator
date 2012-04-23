fcdff78d0193e087770e98a8992eee55167103ec
Chester Husk III <chusk3@gmail.com>
2012-04-23 18:20:13 -0500

    tweaked full-circle test

--- decode.asm
+++ decode.asm
@@ -3,10 +3,11 @@ main:
 			la $a0, input_string			# System call to display string
 			li $v0, 4				# These lines can also be left out
 			la $t0, input_string			# $t0
+			addi $t2, $t2, 2			# sub only works on registers, so I have to store the 2 to subtract in one.
 loop:
 			lb $t1, 0($t0)				# Starts loop
 			beqz $t1, exit_loop			# If null terminator, exit loop
-			subi $t1, $t1, 2   			# Scramble letter
+			sub $t1, $t1, $t2   			# Scramble letter
        		sb $t1, 0($t0)				# Store scrambled letter
 no_change:
 			addiu $t0, $t0, 1			# Increment pointer
